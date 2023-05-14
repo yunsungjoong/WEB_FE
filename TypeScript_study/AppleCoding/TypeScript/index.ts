39e83e7d9ced2ac0fce98e5d800a19b8d17a0cc5
Y-SungJoong <yun_sj94@naver.com>
2023-05-14 22:44:57 +0900

    추가

--- TypeScript_study/AppleCoding/TypeScript/index.ts
+++ TypeScript_study/AppleCoding/TypeScript/index.ts
@@ -1,6 +1,30 @@
-type PositionX = { x : number };
-type PositionY = { y : number};
+/* HTML조작시 narrowing 방법 5개 */
 
-type NewType = PositionX & PositionY;
+// let 제목 = document.querySelector('#title');
+// if (제목 != null){
+// 제목.innerHTML = '반가워요'
+// }
 
-let position : NewType = { x : 10 , y : 20};
+
+/* 2. instanceof 연산자 */
+// if (제목 instanceof Element){
+//     제목.innerHTML = '반가워요'
+// }
+
+/* 3. as로 사기치기 */ 
+// let 제목 = document.querySelector('#title123') as Element;
+// 제목.innerHTML = '반가워요'
+
+
+/* 4. 오브젝트에 붙이는 */
+let 제목 = document.querySelector('#title')
+if (제목?.innerHTML != undefined){  // narrowing 인정해줌   ?은 있으면 출력해주고 없으면 undefined로 
+    제목.innerHTML = '반가워요 '
+}
+
+/* 5. */
+// tscofig.json에서 strict 모드를 끄면 됨 
+
+
+
+// 
