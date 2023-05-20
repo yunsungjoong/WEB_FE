d2dced02f33717fd25e43ef92e60f73017c49a5e
yunsungjoong <yun_sj94@naver.com>
2023-05-20 22:08:41 +0900

    수정

--- React_study/DreamCoding/ReactCloneCoding/react/basic/src/comeponents/Profile.jsx
+++ React_study/DreamCoding/ReactCloneCoding/react/basic/src/comeponents/Profile.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-export default function Profile({image,name,title}) {
+export default function Profile({image,name,title,isNew}) {
     return (
         <div className='profile'>
             <img 
             className='photo' src={image} alt="avatar"/>
+            {isNew && <span className='new'>New</span>}
             <h1>{name}</h1>
             <p>{title}</p>
+            
         </div>
+
     );
 }
 
