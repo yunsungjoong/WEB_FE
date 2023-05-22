5c696f9be649edcbd6391ac3ec087645a77368e4
yunsungjoong <yun_sj94@naver.com>
2023-05-22 22:56:04 +0900

    수정

---
+++ React_study/DreamCoding/ReactCloneCoding/react/basic/src/AppCounter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Counter from './comeponents/Counter';
+
+export default function AppCounter() {
+    return (
+        <div>
+            <Counter />
+        </div>
+    );
+}
+
