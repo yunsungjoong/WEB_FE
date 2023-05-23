b99d39232dea40bd2c2d99b16ce09554a321b1bf
yunsungjoong <yun_sj94@naver.com>
2023-05-23 23:28:13 +0900

    추가수정

--- React_study/DreamCoding/ReactCloneCoding/react/basic/src/AppCounter.jsx
+++ React_study/DreamCoding/ReactCloneCoding/react/basic/src/AppCounter.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Counter from './comeponents/Counter';
+import Counter from './comeponents/Counter'
 
 export default function AppCounter() {
-    return (
-        <div>
-            <Counter />
-        </div>
-    );
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount((prev) => prev + 1)
+  return (
+    <div className='container'>
+      <div className='banner'>
+        Total Count: {count} {count > 10 ? '🔥' : '❄️'}
+      </div>
+      <div className='counters'>
+        <Counter total={count} onClick={handleClick} />
+        <Counter total={count} onClick={handleClick}/>
+      </div>
+    </div>
+  );
 }
-
--- React_study/DreamCoding/ReactCloneCoding/react/basic/src/comeponents/Counter.jsx
+++ React_study/DreamCoding/ReactCloneCoding/react/basic/src/comeponents/Counter.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 
-
-
-export default function Counter() {
+export default function Counter({total, onClick}) {
     const [count, setCount] = useState(0);
     
     return (
         <div className='counter'>
-            <span className='number'>{count}</span>
+            <p className='number'>{count} <span className='total'>/{total}</span> </p>
             <button className='button' 
             onClick={()=>{
-                setCount(count + 1)
-            }}>Add +</button>
+                // setCount(count + 1)
+                setCount((prev)=> prev +1);
+                onClick();
+            }}
+            >
+                Add +
+            </button>
         </div>
     );
 }
