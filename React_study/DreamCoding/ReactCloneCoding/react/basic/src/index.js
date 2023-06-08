61e3023bc087344d305798f9ca35f842c182ffbb
yunsungjoong <yun_sj94@naver.com>
2023-06-08 23:23:10 +0900

    추가

--- React_study/DreamCoding/ReactCloneCoding/react/basic/src/index.js
+++ React_study/DreamCoding/ReactCloneCoding/react/basic/src/index.js
@@ -14,7 +14,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <AppMentors></AppMentors> */}
-    <AppForm></AppForm>
+    {/* <AppForm></AppForm> */}
+    <AppWrap />
   </React.StrictMode>
 );
 
@@ -22,3 +23,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+///
