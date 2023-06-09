1952749aaadbb852416d4d6002f4a4d668dbe675
Y-SungJoong <yun_sj94@naver.com>
2023-06-09 15:54:53 +0900

    추가

--- React_study/DreamCoding/ReactCloneCoding/react/basic/src/AppWrap.jsx
+++ React_study/DreamCoding/ReactCloneCoding/react/basic/src/AppWrap.jsx
@@ -3,20 +3,21 @@ import React from 'react';
 export default function AppWrap() {
     return (
         <div>
-            <NavBar />
+            <NavBar>
+                <Avatar
+                image = 'https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
+                name = 'Bob'
+                size={200}
+                />
+                <p>안녕하세요 ! 모두</p>
+            </NavBar>
         </div>
     );
 }
 
-function NavBar() {
+function NavBar({children}) {
     return (
-        <header style ={{ backgroundColor: 'yellow'}}>
-            <Avatar
-            image = 'https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
-            name = 'Bob'
-            size={200}
-            />
-        </header>
+        <header style ={{ backgroundColor: 'yellow'}}>{children}</header>
     );
 }
 
--- React_study/DreamCoding/ReactCloneCoding/react/basic/src/index.js
+++ React_study/DreamCoding/ReactCloneCoding/react/basic/src/index.js
@@ -9,13 +9,14 @@ import reportWebVitals from './reportWebVitals';
 // import AppMentor from './AppMentor';
 // import AppMentors from './AppMentors';
 import AppForm from './AppForm'
+import AppWrap from './AppWrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <AppMentors></AppMentors> */}
     {/* <AppForm></AppForm> */}
-    <AppWrap />
+    <AppWrap></AppWrap>
   </React.StrictMode>
 );
 
