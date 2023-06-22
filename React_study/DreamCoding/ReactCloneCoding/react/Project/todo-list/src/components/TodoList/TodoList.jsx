39eb23315e40819cd23da47e8d0e4a97655ecfe0
yunsungjoong <yun_sj94@naver.com>
2023-06-22 23:14:47 +0900

    ㅇ

--- React_study/DreamCoding/ReactCloneCoding/react/Project/todo-list/src/components/TodoList/TodoList.jsx
+++ React_study/DreamCoding/ReactCloneCoding/react/Project/todo-list/src/components/TodoList/TodoList.jsx
@@ -2,38 +2,38 @@ import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 
-export default function TodoList( filter ) {
-    const [todos, setTodos] = useState([
-        {id: '123', text : '장보기', status:'active'},
-        {id: '124', text : '공부하기', status:'active'},
-    ]);
-    const handleAdd = (todo) => setTodos([...todos, todo])
-    const handleUpdate = (updated) => 
-        setTodos(todos.map((t) => t.id === updated.id ? updated : t));
-    const handleDelete = (deleted) => 
-        setTodos(todos.filter((t) => t.id !== deleted.id));
+export default function TodoList({ filter }) {
+  const [todos, setTodos] = useState([
+    { id: '123', text: '장보기', status: 'active' },
+    { id: '124', text: '공부하기', status: 'active' },
+  ]);
+  const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((t) => t.id !== deleted.id));
 
-    const filtered = getFilteredItems(todos, filter)
-    return ( 
-        <section>
-            <ul>
-                {filtered.map((item) => (
-                    <Todo 
-                        key={item.id} 
-                        todo={item} 
-                        onUpdate={handleUpdate}
-                        onDelete={handleDelete}
-                    />
-                ))}
-            </ul>
-            <AddTodo onAdd={handleAdd} />
-        </section>
-        );
+  const filtered = getFilteredItems(todos, filter);
+  return (
+    <section>
+      <ul>
+        {filtered.map((item) => (
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
+        ))}
+      </ul>
+      <AddTodo onAdd={handleAdd} />
+    </section>
+  );
 }
 
 function getFilteredItems(todos, filter) {
-    if(filter === 'all') {
-        return todos;
-    }
-    return todos.filter((todo) => todo.status === filter);
+  if (filter === 'all') {
+    return todos;
+  }
+  return todos.filter((todo) => todo.status === filter);
 }
