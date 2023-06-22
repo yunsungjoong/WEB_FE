ddf1c33fe3d279e0242b88b6607f2b71cc2d6d8b
yunsungjoong <yun_sj94@naver.com>
2023-06-22 23:12:32 +0900

    수정

--- React_study/DreamCoding/ReactCloneCoding/react/Project/todo-list/src/components/TodoList/TodoList.jsx
+++ React_study/DreamCoding/ReactCloneCoding/react/Project/todo-list/src/components/TodoList/TodoList.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
-export default function TodoList() {
+
+export default function TodoList( filter ) {
     const [todos, setTodos] = useState([
         {id: '123', text : '장보기', status:'active'},
         {id: '124', text : '공부하기', status:'active'},
@@ -11,10 +12,12 @@ export default function TodoList() {
         setTodos(todos.map((t) => t.id === updated.id ? updated : t));
     const handleDelete = (deleted) => 
         setTodos(todos.filter((t) => t.id !== deleted.id));
+
+    const filtered = getFilteredItems(todos, filter)
     return ( 
         <section>
             <ul>
-                {todos.map((item) => (
+                {filtered.map((item) => (
                     <Todo 
                         key={item.id} 
                         todo={item} 
@@ -28,3 +31,9 @@ export default function TodoList() {
         );
 }
 
+function getFilteredItems(todos, filter) {
+    if(filter === 'all') {
+        return todos;
+    }
+    return todos.filter((todo) => todo.status === filter);
+}
