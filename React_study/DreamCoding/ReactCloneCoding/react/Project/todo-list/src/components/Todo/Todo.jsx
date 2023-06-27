f80ab92fc84905773f607064ed34838af4b33c6a
Y-SungJoong <yun_sj94@naver.com>
2023-06-27 12:53:13 +0900

    추가

--- React_study/DreamCoding/ReactCloneCoding/react/Project/todo-list/src/components/Todo/Todo.jsx
+++ React_study/DreamCoding/ReactCloneCoding/react/Project/todo-list/src/components/Todo/Todo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FaTrashAlt} from 'react-icons/fa';
-
+import styles from './Todo.module.css'
 export default function Todo({ todo, onUpdate, onDelete }) {
     const {text, status} = todo;
     const handleChange = (e) => {
@@ -8,19 +8,25 @@ export default function Todo({ todo, onUpdate, onDelete }) {
         onUpdate({...todo, status })
     }
     const handleDelete = () => onDelete(todo);
-    return (<li>
-        <input 
-            type='checkbox' 
-            id='checkbox' 
-            checked={status === 'completed'} 
-            onChange={handleChange}
-            />
-            
-        <label htmlFor='checkbox'>{text}</label>
-        <button onClick={handleDelete}>
-            <FaTrashAlt />
-        </button>
-    </li>
+    return (
+        <li className={styles.todo}>
+            <input 
+            className={styles.checkbox}
+                type='checkbox' 
+                id='checkbox' 
+                checked={status === 'completed'} 
+                onChange={handleChange}
+                />
+                
+            <label htmlFor='checkbox' className={styles.text}>
+                {text}
+            </label>
+            <span className={styles.icon}>
+                <button onClick={handleDelete} className={styles.button}>
+                    <FaTrashAlt />
+                </button>
+            </span>
+        </li>
     );
 }
 
