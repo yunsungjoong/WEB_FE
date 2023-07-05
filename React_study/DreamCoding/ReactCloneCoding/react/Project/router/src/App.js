a688abd6071f19a23527422187020f98346bd179
Y-SungJoong <yun_sj94@naver.com>
2023-07-05 15:58:31 +0900

    dd

--- React_study/DreamCoding/ReactCloneCoding/react/Project/router/src/App.js
+++ React_study/DreamCoding/ReactCloneCoding/react/Project/router/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {createBrowserRouter , RouterProvider} from 'react-router-dom';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>Home</p>,
+    errorElement: <p>Not Found🥲</p>
+  
+  },
+  {
+    path: '/videos',
+    element: <p>Videos</p>,
+  },
+]);
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
