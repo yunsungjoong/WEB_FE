8ce61f564689fd288a0b90547104630b4a2fa8f7
Y-SungJoong <yun_sj94@naver.com>
2023-07-14 17:34:21 +0900

    유튜브 경로 or test json 부분 수정

--- React_study/DreamCoding/ReactCloneCoding/react/Project/youtube/src/pages/Videos.jsx
+++ React_study/DreamCoding/ReactCloneCoding/react/Project/youtube/src/pages/Videos.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
-import axios from 'axios';
+import Youtube from '../api/youtube';
+import FakeYoutube from '../api/fakeYoutube';
+
 
 export default function Videos() {
   const { keyword } = useParams();
@@ -10,11 +12,9 @@ export default function Videos() {
     isLoading,
     error,
     data: videos,
-  } = useQuery(['videos', keyword], async () => {
-    return axios
-        .get(`/videos/${keyword ? 'search' : 'popular'}.json`)
-        .then((res) => res.data.items);
-      
+  } = useQuery(['videos', keyword], () => {
+    const youtube = new FakeYoutube();
+    return youtube.search(keyword);
   });
   return (
     <>
