03faeeeb054403fab13260900fc97ae5c2cc1f52
yunsungjoong <yun_sj94@naver.com>
2023-12-04 17:12:42 +0900

    넷플릭스 작업

--- Vue_study/netflix-clone/src/App.vue
+++ Vue_study/netflix-clone/src/App.vue
@@ -1,5 +1,19 @@
 <script setup>
+import { onMounted, ref } from 'vue';
 
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+
+import HomeOutline from 'vue-material-design-icon/HomeOutline.vue';
+import TrendingUp from 'vue-material-design-icon/TrendingUp.vue';
+import Television from 'vue-material-design-icon/Television.vue';
+import MovieOutline from 'vue-material-design-icon/MovieOutline.vue';
+import Plus from 'vue-material-design-icon/Plus.vue';
+import ChevronLeft from 'vue-material-design-icon/ChevronLeft.vue';
+
+import { useMovieStore } from './stores/movie'
+import { storeToRefs } from 'pinia';
+const useMovie = useMovieStore()
+const { movie, showFullVideo } = storeToRefs(useMovie)
 </script>
 
 <template>
