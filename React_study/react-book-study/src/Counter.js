155a5faff0eb0cda099148edd380eb72c89c8aca
yunsungjoong <yun_sj94@naver.com>
2024-02-09 23:52:15 +0900

    .

--- React_study/react-book-study/src/Counter.js
+++ React_study/react-book-study/src/Counter.js
@@ -5,16 +5,20 @@ class Counter extends Component {
         super(props);
         // state의 초기값 설정하기
         this.state = {
-            number: 0
+            number: 0,
+            fixedNumber: 0
         };
     }
     render() {
-        const { number } = this.state; // state를 조회할 때는 this.state로 조회합니다.
+        const { number , fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회합니다.
         return (
             <div>
                 <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixedNumber}</h2>
                 <button 
+                    //onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정한다.
                     onClick={() => { 
+                        // this.setState를 사용해 state에 새로운 값을 넣을 수 있다.
                         this.setState({ number: number + 1 })
                     }}
                 >
