15ea2430771d1419a9ea4a0050ab1d26adae6fd1
yunsungjoong <yun_sj94@naver.com>
2024-02-14 22:26:05 +0900

    클래스 컴포넌트 ref 사용방법

--- React_study/react-book-study/src/App.js
+++ React_study/react-book-study/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
 
 class App extends Component {
     
     render() {
         return (
             <div>
-                <EventPractice/>
+                <ValidationSample/>
             </div>
         );
     }
