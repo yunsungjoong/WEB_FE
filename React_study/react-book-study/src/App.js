a2f2e9f01c0d21950524c07e8904b0512c64e2f0
yunsungjoong <yun_sj94@naver.com>
2024-02-15 23:55:33 +0900

    .

--- React_study/react-book-study/src/App.js
+++ React_study/react-book-study/src/App.js
@@ -6,7 +6,10 @@ class App extends Component {
     render() {
         return (
             <div>
-                <ScrollBox />
+                <ScrollBox ref={(ref) => this.ScrollBox=ref} />
+                <button onClick={() => this.ScrollBox.scrollToBottom()}>
+                    맨 밑으로
+                </button>
             </div>
         );
     }
