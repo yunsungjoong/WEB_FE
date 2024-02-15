0fd2f94a1593f7db8f61442f244f137f1bbe1f48
yunsungjoong <yun_sj94@naver.com>
2024-02-15 20:19:29 +0900

    App 컴포넌트에서 스크롤 박스 컴포넌트 렌더링

--- React_study/react-book-study/src/App.js
+++ React_study/react-book-study/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
     
     render() {
         return (
             <div>
-                <ValidationSample/>
+                <ScrollBox />
             </div>
         );
     }
