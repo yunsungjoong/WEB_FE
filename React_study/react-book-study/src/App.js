ac9cb9809ed972e3b60895e5d121644cd70151e2
yunsungjoong <yun_sj94@naver.com>
2024-02-18 21:51:59 +0900

    ..

--- React_study/react-book-study/src/App.js
+++ React_study/react-book-study/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import IterationSample from './IterationSample';
+import LiffeCycleSample from './LiffeCycleSample';
+
+
+//랜덤 색상을 생성합니다.
+
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
 
 class App extends Component {
-    
+    state = { color: '#000000'}   
+
+    handleClick = () => {
+        this.setState({
+            color: getRandomColor()
+        });
+    }
     render() {
         return (
             <div>
-                <IterationSample />
+                <button onClick={this.handleClick}>랜덤 색상</button>
+                <LiffeCycleSample color={this.state.color} />
             </div>
         );
     }
