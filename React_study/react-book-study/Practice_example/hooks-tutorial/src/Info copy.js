85edf4ffa894a9cddecc692b7283d551321eea8c
yunsungjoong <yun_sj94@naver.com>
2024-02-21 19:36:14 +0900

    useReucer 사용해보기

---
+++ React_study/react-book-study/Practice_example/hooks-tutorial/src/Info copy.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+
+const Info = () => {
+    const [name, setName] = useState('')
+    const [nickname, setNickname] = useState('');
+
+    useEffect(() => {
+        console.log('effect');
+        return() => {
+            console.log('unmount');
+        };
+    }, []);
+
+    
+    
+    const onChangeName = e => {
+        setName(e.target.value);
+    };
+
+    const onChangeNickName = e => {
+        setNickname(e.target.value);
+    };
+
+    return (
+        <div>
+            <div>
+                <input value={name} onChange={onChangeName} />
+                <input value={nickname} onChange={onChangeNickName}/>
+            </div>
+            <div>
+                <div>
+                    <b>이름:</b> {name}
+                </div>
+                <div>
+                    <b>닉네임:</b> {nickname}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Info;
--- React_study/react-book-study/Practice_example/hooks-tutorial/src/Info.js
+++ React_study/react-book-study/Practice_example/hooks-tutorial/src/Info.js
@@ -1,38 +1,37 @@
-import { useState, useEffect } from 'react';
+import { useReducer } from 'react';
+
+function reducer(state, action) {
+    return {
+        ...state,
+        [action.name]: action.value
+    };
+}
+
 
 const Info = () => {
-    const [name, setName] = useState('')
-    const [nickname, setNickname] = useState('');
-
-    useEffect(() => {
-        console.log('effect');
-        return() => {
-            console.log('unmount');
-        };
-    }, []);
-
-    
-    
-    const onChangeName = e => {
-        setName(e.target.value);
+    const [state, dispatch] = useReducer(reducer, {
+        name: '',
+        nickname: ''
+    });
+    const { name, nickname } = state;
+    const onChange = e => {
+        dispatch(e.target);
     };
 
-    const onChangeNickName = e => {
-        setNickname(e.target.value);
-    };
 
     return (
         <div>
             <div>
-                <input value={name} onChange={onChangeName} />
-                <input value={nickname} onChange={onChangeNickName}/>
+                <input name="name" value={name} onChange={onChange} />
+                <input name="nickname" value={nickname} onChange={onChange}/>
             </div>
             <div>
                 <div>
                     <b>이름:</b> {name}
                 </div>
                 <div>
-                    <b>닉네임:</b> {nickname}
+                    <b>닉네임:</b> 
+                    {nickname}
                 </div>
             </div>
         </div>
