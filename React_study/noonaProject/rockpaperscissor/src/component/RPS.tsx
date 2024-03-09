e07495d7cfce14418c304d2830bc7a2b3180b86d
yunsungjoong <yun_sj94@naver.com>
2024-03-10 00:37:15 +0900

    첫 시작

---
+++ React_study/noonaProject/rockpaperscissor/src/component/RPS.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface Scores {
+    [key: string]: number;
+}
+
+const scores: Scores = {
+    '✊': 1,
+    '✋': 1,
+    '✌': 1,
+};
+
+console.log(scores)
+
+
+const RPS = () => {
+    
+    return (
+        <div>
+            sadsad
+        </div>
+    );
+};
+
+export default RPS;
