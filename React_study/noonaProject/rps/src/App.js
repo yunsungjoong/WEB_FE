a5eafdd5c211fddb663b20e00602ab5475faa228
yunsungjoong <yun_sj94@naver.com>
2024-03-13 00:05:10 +0900

    React 오리지널 가위바위보 복습

--- React_study/noonaProject/rps/src/App.js
+++ React_study/noonaProject/rps/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Box from './component/Box';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const choise = {
+  rock: {
+    name:"Rock",
+    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVZohJyhyZNNMTYOf7_E16KklxhKnoZQYkwLv1pEgmOfesxr5Akzd7ydV2ad-ECgoRhCM&usqp=CAU"
+  },
+  scissors: {
+    name:"Scissors",
+    img:"https://blackbearwow.github.io/image/rockPaperScissors/scissors.png"
+  },
+  paper: {
+    name:"Paper",
+    img:"https://blog.kakaocdn.net/dn/bmjB2s/btqXHhp6kpG/TH14W4U612SxKo9uuR2sB0/img.png"
+  },
 }
+
+const App = () => {
+  const play = ( userChioce ) => {
+    console.log("선택된 아이들입니다." , userChioce)
+  }
+  return (
+    <div>
+      <div className='main'>
+        <Box title="You" />
+        <Box title="Computer"/>
+      </div>
+      <div className='rps-button'>
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
+      </div>
+    </div>
+
+  );
+};
 
 export default App;
---
+++ React_study/noonaProject/rps/src/component/Box.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Box = ( props ) => {
+    return (
+        <div className='box'>
+            <h1>{props.title}</h1>
+            <img className="item-img" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVZohJyhyZNNMTYOf7_E16KklxhKnoZQYkwLv1pEgmOfesxr5Akzd7ydV2ad-ECgoRhCM&usqp=CAU'/>
+            <h2>{props.title}</h2>
+{/* 가위  https://blackbearwow.github.io/image/rockPaperScissors/scissors.png */}
+{/* 빠 : https://blog.kakaocdn.net/dn/bmjB2s/btqXHhp6kpG/TH14W4U612SxKo9uuR2sB0/img.png */}
+
+        </div>
+    );
+};
+
+export default Box;
