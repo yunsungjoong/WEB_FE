7ac5153ce4c208b4d1e6e4333446e62288595d85
yunsungjoong <yun_sj94@naver.com>
2024-03-15 23:36:00 +0900

    위도와 경도 값을 추출 하고 현재위치 값을 위도 경도로 찾기 완료

--- React_study/noonaProject/weather-app/src/App.js
+++ React_study/noonaProject/weather-app/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  
+  const getCurrentLocation = () => {
+    //현재 위치 가져오기
+    
+  
+    navigator.geolocation.getCurrentPosition(( position ) => {
+      const API_key = '20690d0edaf5b7153c40c4608209523a'
+      let lat = position.coords.latitude; // 위도
+      let lon = position.coords.longitude; // 경도
+      
+      console.log('현재위치', lat, lon);
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`
+      console.log('현재위치 날씨', url)
+    });
+  }
+
+  // Api로 현재 위치 가져오기 해보기 
+  useEffect(() => { 
+    getCurrentLocation();
+  }, []); // componentDidMount() []이면 화면에 그리고 나서 바로 실행된다.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // 뭘할지 모르겠다면 스텝바이 스텝으로 정리해둔대로 시작한다.
+    // 1. 앱이 실행하면 현재위치 기반의 날씨가 보인다.
+    // 2. 날씨정보에는 도시, 섭씨 , 화씨 , 날씨상태
+    // 3. 5개의 버튼이 있다 ( 1개는 다른 도시 )
+    // 4. 현재위치 버튼을 누르면 현재위치로 돌아간다.
+    <div></div>
   );
 }
 
