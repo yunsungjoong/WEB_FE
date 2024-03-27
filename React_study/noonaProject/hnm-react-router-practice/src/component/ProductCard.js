16dd83a4ed1a2a2633df749b46d772e01ff02f58
yunsungjoong <yun_sj94@naver.com>
2024-03-28 00:25:07 +0900

    json에서 가지고 온 데이터를 통해 화면에 이미지 뿌리기

---
+++ React_study/noonaProject/hnm-react-router-practice/src/component/ProductCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProductCard = () => {
+    return (
+        <div>
+            
+            <img src="https://lp2.hm.com/hmgoepprod?set=source[/e1/28/e1287643ff5f9fb6238a73d28a8a534c49b02f7d.jpg],origin[dam],category[],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]" alt="Conscious Choice" />
+                <div>conscious choice</div>
+                <div>벨티드 트윌 코트</div>
+                <div>$9999000</div>
+                <div>신제품</div>
+            
+        </div>
+    );
+};
+
+export default ProductCard;
--- React_study/noonaProject/hnm-react-router-practice/src/page/ProductAll.js
+++ React_study/noonaProject/hnm-react-router-practice/src/page/ProductAll.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
+    const [productList, setProductList] = useState([]);
+    const getProducts =  async() => { 
+        let url = `http://localhost:5005/products`
+        let response = await fetch(url);
+        let data = await response.json();
+        setProductList(data)
+        
+    };
+    useEffect(() => {
+        getProducts()
+    },[])
     return (
-        <div></div>
+        <div>
+            <ProductCard></ProductCard>
+
+        </div>
     );
 };
 
