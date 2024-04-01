571a3cc3083e2df9b0b24df0eb8f292d1933302d
yunsungjoong <yun_sj94@naver.com>
2024-04-01 23:16:24 +0900

    수정

--- React_study/noonaProject/hnm-react-router-practice/src/page/ProductDetail.js
+++ React_study/noonaProject/hnm-react-router-practice/src/page/ProductDetail.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
-    return (
-        <div>상품상세페이지</div>
-    );
+    let {id} = useParams()
+    const [product , setProduct] = useState(null)
+    const getProductDetail = async () => {
+        let url = `http://localhost:5005/products/${id}`
+        let response = await fetch(url)
+        let data = await response.json()
+        console.log(data)
+        setProduct(data)
+    }
+    
+    useEffect(() => {
+        getProductDetail();
+    }, [])
+    return <Container>
+        <Row>
+            <Col className='product-img'>
+                <img src={product?.img}/>
+            </Col>
+            <Col>
+                <div>{product?.title}</div>
+                <div>{product?.price}</div>
+            </Col>
+        </Row>
+        </Container>
 };
 
 export default ProductDetail;
